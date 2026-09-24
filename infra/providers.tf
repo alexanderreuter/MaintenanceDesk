64fefe834c2778afb4916d22cd424a4a3630a3dc
Alexander Reuter <145650568+alexanderreuter@users.noreply.github.com>
2026-09-24 15:39:29 +0200

    Let Terraform delete the resource group App Insights adds to

--- infra/providers.tf
+++ infra/providers.tf
@@ -26,7 +26,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 
   resource_providers_to_register = [
     "Microsoft.App",
