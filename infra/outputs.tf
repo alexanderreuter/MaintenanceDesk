0bd2ba71d982f2a9a6ed94da4f851a47da903c4e
Alexander Reuter <145650568+alexanderreuter@users.noreply.github.com>
2026-09-24 15:40:09 +0200

    Output the API URL, which changes on every rebuild

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "registry_login_server" {
   description = "Prefix for image names pushed to the registry."
   value       = azurerm_container_registry.main.login_server
 }
+
+output "api_url" {
+  description = "Public HTTPS address of the API."
+  value       = "https://${azurerm_container_app.api.ingress[0].fqdn}"
+}
